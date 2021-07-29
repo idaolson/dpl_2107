fff7ce87609ccfe191e96e75a432633691b43437
Ida Olson <idaolson@gmail.com>
2021-07-29 13:37:30 -0600

    Complete iteration 2

---
+++ lib/author.rb
@@ -0,0 +1,22 @@
+class Author
+  attr_reader :first_name,
+              :last_name,
+              :books
+
+  def initialize(params)
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @books = []
+  end
+
+  def name
+    @first_name + " " + @last_name
+  end
+
+  def write(title, pub_date)
+    Book.new({author_first_name: @first_name,
+              author_last_name: @last_name,
+              title: title,
+              publication_date: pub_date})
+  end
+end
